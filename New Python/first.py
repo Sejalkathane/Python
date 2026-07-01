efbbbe551e54576a0d28ec2578be1b6e1841dfc0
Sejal <kathanesejal@gmail.com>
2026-07-02 00:36:55 +0530

    learn var

---
+++ New Python/first.py
@@ -0,0 +1,42 @@
+print("Hello, World!")
+# Comment
+
+# variable declaration
+
+# STRING
+x="BRO";
+email= "bro@example.com"
+print("Hello "+  x);
+print(f"hello {x}");
+print(f"hello {x} and {x}");
+print(f"my email is {email}");
+
+# use of f::: We use f so we can put variables directly inside a string, instead of joining pieces together manually.
+
+
+# integer
+x= 25;
+y= 3;
+print(f"your age is {x} and buy {y} oranges");
+
+
+# FLOAT
+price= 25.5;
+print(f"my price is {price}");
+
+# Boolean
+is_student=False;
+print(f"Is student: {is_student}");
+
+if is_student:
+    print("I am a student");
+else :
+ print("I am not a student");
+ 
+
+
+
+
+
+
+
